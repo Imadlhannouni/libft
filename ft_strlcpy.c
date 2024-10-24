ae261345a3cdde6b46c907533e6044ad7f579318
Imad Lhannouni <ilhannou@c1r9p6.1337.ma>
2024-10-24 15:38:39 +0100

    Initial commit

---
+++ ft_strlcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilhannou <ilhannou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 16:04:50 by ilhannou          #+#    #+#             */
+/*   Updated: 2024/10/24 15:35:14 by ilhannou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	l;
+
+	l = 0;
+	i = 0;
+	while (src[l] != '\0')
+		l++;
+	if (size == 0)
+		return (l);
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (l);
+}
